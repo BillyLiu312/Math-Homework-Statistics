f1785075560caeecfb9401f666d24843e735126f
mengfan_liu <mengfan_liu@BILLYMacBook-Pro.local>
2025-12-09 16:23:30 +0800

    add data collection function

---
+++ data_collect.py
@@ -0,0 +1,13 @@
+import yfinance as yf
+import numpy as np
+import pandas as pd
+import os
+import matplotlib.pyplot as plt
+
+df = yf.download('^GSPC', start='2000-01-01', end='2023-12-31')
+df['Log_Ret'] = np.log(df['Close'] / df['Close'].shift(1))
+df = df.dropna()
+
+# 确保 data 目录存在并保存为 CSV
+os.makedirs('data', exist_ok=True)
+df.to_csv(os.path.join('data', 'SP500_log_returns.csv'), index=True)
